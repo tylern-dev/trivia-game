2f12b6f05ae488c60c2d2193cd7d402dcf8a7329
Tyler Negro <tnegro@gmail.com>
2017-07-14 22:55:19 -0600

    added files

---
+++ assets/scripts/game.js
@@ -0,0 +1,39 @@
+// -------Pseudo Code--------
+//questionTimer
+//answerTimer
+//questionTimeRunning = true/false
+
+//scores
+    // correctAnswer:
+    // wrongAnswer:
+    // unanswers:
+
+//questionArray=[
+//     {
+//         question:
+//         answer:
+//         gifURL:
+//         alerternateAnswers:[]
+
+
+// ]
+
+
+// start game
+//     loop through questions
+//         display question
+//             start question timer
+//             if correct answer
+//                 start answer timer
+//                 display correct screen 
+//                 answer ++
+//             if wrong answer
+//                 start answer timer
+//                 display wrong screen
+//                 wrong ++
+//             if time ends with no answer 
+//                 start answer timer
+//                 display out of time screen
+//                 unanswered ++
+
+
